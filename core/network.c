dcc3921682c8bededfac7d76f4976fd56b051c72
irungentoo <irungentoo@gmail.com>
2013-06-26 15:02:42 -0400

    Licenced the code under the GPL for now.

--- core/network.c
+++ core/network.c
@@ -1,4 +1,26 @@
+/* network.h
+* 
+* Functions for the core networking.
+* 
+ 
+    Copyright (C) 2013 Tox project All Rights Reserved.
 
+    This file is part of Tox.
+
+    Tox is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+    
+*/
 
 #include "network.h"
 
@@ -57,7 +79,7 @@ int init_networking(IP ip ,uint16_t port)
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); 
     
     //Functions to increase the size of the send and recieve UDP buffers
-    //NOTE: uncomment if necessary.
+    //NOTE: uncomment if nn
     /*
     int n = 1024 * 1024 * 2;
     if(setsockopt(sock, SOL_SOCKET, SO_RCVBUF, (char*)&n, sizeof(n)) == -1)
