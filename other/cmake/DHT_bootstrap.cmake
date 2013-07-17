ef0efd72b51a44d30272dccffdc365165d385291
Maxim Biro <nurupo.contributions@gmail.com>
2013-07-17 18:06:05 -0400

    Improved build system

---
+++ other/cmake/DHT_bootstrap.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 2.6.0)
+project(DHT_bootstrap C)
+
+set(exe_name DHT_bootstrap)
+
+add_executable(${exe_name}
+        DHT_bootstrap.c)
+
+linkCoreLibraries(${exe_name})
+addCompilerFlags()
---
+++ testing/cmake/DHT_test.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 2.6.0)
+project(DHT_test C)
+
+set(exe_name DHT_test)
+
+add_executable(${exe_name}
+        DHT_test.c)
+
+linkCoreLibraries(${exe_name})
+addCompilerFlags()
