f377eb9ca26fc1d476938b441bb256837bbee4f5
SilentSand <edb881@gmail.com>
2013-07-25 21:45:56 -0400

    Licensing
    Added the GPLv3 license to some files in addition to fixing some
    comments at the beginning of the files.

--- core/Lossless_UDP.h
+++ core/Lossless_UDP.h
@@ -1,26 +1,25 @@
 /* Lossless_UDP.h
-* 
-* An implementation of the Lossless_UDP protocol as seen in docs/Lossless_UDP.txt
-* 
- 
-    Copyright (C) 2013 Tox project All Rights Reserved.
-
-    This file is part of Tox.
-
-    Tox is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Tox is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Tox.  If not, see <http://www.gnu.org/licenses/>.
-    
-*/
+ * 
+ * An implementation of the Lossless_UDP protocol as seen in docs/Lossless_UDP.txt
+ * 
+ *  Copyright (C) 2013 Tox project All Rights Reserved.
+ *
+ *  This file is part of Tox.
+ *
+ *  Tox is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
 
 #ifndef LOSSLESS_UDP_H 
 #define LOSSLESS_UDP_H  
--- testing/DHT_test.c
+++ testing/DHT_test.c
@@ -7,7 +7,26 @@
  * EX: ./test 127.0.0.1 33445 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
  * 
  * The test will then ask you for the id (in hex format) of the friend you wish to add
+ *
+ *  Copyright (C) 2013 Tox project All Rights Reserved.
+ *
+ *  This file is part of Tox.
+ *
+ *  Tox is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+ *  
  */
+ 
 //#include "../core/network.h"
 #include "../core/DHT.c"
 #include "../core/friend_requests.c"
