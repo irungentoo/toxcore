071ac463082646189ade6e31bb3f6051516f81b2
plutooo <tfy12vbr@student.lu.se>
2013-08-05 12:51:58 -0700

    core: Rewrote ping-module, better performance and cleaner code.

---
+++ core/util.c
@@ -0,0 +1,31 @@
+/*
+ * util.c -- Utilities.
+ *
+ * This file is donated to the Tox Project.
+ * Copyright 2013  plutooo
+ */
+
+#include <time.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "network.h"
+
+uint64_t now() {
+  return time(NULL);
+}
+
+uint64_t random_64b() {
+  uint64_t r;
+
+  // This is probably not random enough?
+  r = random_int();
+  r <<= 32;
+  r |= random_int();
+
+  return r;
+}
+
+bool ipp_eq(IP_Port a, IP_Port b) {
+  return (a.ip.i == b.ip.i) && (a.port == b.port);
+}
