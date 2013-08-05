99d9ecf74cab43ca6ae24ab5dd62a3b758b3e666
plutooo <tfy12vbr@student.lu.se>
2013-08-05 15:04:38 -0700

    core: Move send ping packets functions to ping.c

--- core/ping.c
+++ core/ping.c
@@ -8,6 +8,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "DHT.h"
+#include "net_crypto.h"
+#include "packets.h"
 #include "network.h"
 #include "util.h"
 
@@ -20,9 +23,12 @@ typedef struct {
   uint64_t timestamp;
 } pinged_t;
 
-static pinged_t pings[PING_NUM_MAX];
-static size_t   num_pings;
-static size_t   pos_pings;
+static pinged_t    pings[PING_NUM_MAX];
+static size_t      num_pings;
+static size_t      pos_pings;
+static clientid_t* self_id = (clientid_t*) &self_public_key;
+
+extern uint8_t self_secret_key[crypto_box_SECRETKEYBYTES]; // DHT.c
 
 
 void init_ping() {
@@ -86,10 +92,63 @@ bool is_pinging(IP_Port ipp, uint64_t ping_id) { // O(n)
   for(i=0; i<num_pings; i++) {
     id = (pos_pings + i) % PING_NUM_MAX;
 
-    if(ipp_eq(pings[id].ipp, ipp) && pings[id].id == ping_id) {
+    // ping_id = 0 means match any id
+    if(ipp_eq(pings[id].ipp, ipp) && (ping_id == 0 || pings[id].id == ping_id)) {
       return true;
     }
   }
 
   return false;
+}
+
+int send_ping_request(IP_Port ipp, clientid_t* client_id) {
+  pingreq_t pk;
+  int       rc;
+  uint64_t  ping_id;
+
+  if(is_pinging(ipp, 0) || id_eq(client_id, self_id))
+    return 1;
+
+  // Generate random ping_id
+  ping_id = add_ping(ipp);
+
+  pk.magic = PACKET_PING_REQ;
+  id_cpy(&pk.client_id, self_id);     // Our pubkey
+  random_nonce((uint8_t*) &pk.nonce); // Generate random nonce
+
+  // Encrypt ping_id using recipient privkey
+  rc = encrypt_data((uint8_t*) client_id,
+                    self_secret_key,
+                    (uint8_t*) &pk.nonce,
+                    (uint8_t*) &ping_id, sizeof(ping_id),
+                    (uint8_t*) &pk.ping_id);
+
+  if(rc != sizeof(ping_id) + ENCRYPTION_PADDING)
+    return 1;
+
+  return sendpacket(ipp, (uint8_t*) &pk, sizeof(pk));
+}
+
+int send_ping_response(IP_Port ipp, clientid_t* client_id, uint64_t ping_id) {
+  pingres_t pk;
+  int       rc;
+
+  if(id_eq(client_id, self_id))
+    return 1;
+
+  pk.magic = PACKET_PING_RES;
+  id_cpy(&pk.client_id, self_id);     // Our pubkey
+  random_nonce((uint8_t*) &pk.nonce); // Generate random nonce
+
+  // Encrypt ping_id using recipient privkey
+  rc = encrypt_data((uint8_t*) client_id,
+                    self_secret_key,
+                    (uint8_t*) &pk.nonce,
+                    (uint8_t*) &ping_id, sizeof(ping_id),
+                    (uint8_t*) &pk.ping_id);
+
+  if(rc != sizeof(ping_id) + ENCRYPTION_PADDING)
+    return 1;
+
+  return sendpacket(ipp, (uint8_t*) &pk, sizeof(pk));
 }
