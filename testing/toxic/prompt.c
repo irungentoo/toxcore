b32a7b2294fff9f5ef0f5027e4e2ce1f01adc221
ptrasd <f@k.e>
2013-08-07 02:10:46 -0400

    testing/toxic/prompt.c: changed execute() to skip whitespace at start of command

--- testing/toxic/prompt.c
+++ testing/toxic/prompt.c
@@ -52,6 +52,11 @@ static void execute(ToxWindow *self, char *u_cmd)
       cmd[i - newlines] = u_cmd[i];
   }
 
+  int leading_spc = 0;
+  for (i = 0; i < 256 && isspace(cmd[i]); ++i)
+    leading_spc++;
+  memmove(cmd, cmd + leading_spc, 256 - leading_spc);
+
   if (!strcmp(cmd, "quit") || !strcmp(cmd, "exit") || !strcmp(cmd, "q")) {
     endwin();
     exit(0);
