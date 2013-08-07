caa75b52c37b16aa00f1cb692cc533444c5d716d
ptrasd <f@k.e>
2013-08-07 02:25:20 -0400

    testing/toxic/prompt.c: changed execute() to ignore whitespace at end of commands

--- testing/toxic/prompt.c
+++ testing/toxic/prompt.c
@@ -57,6 +57,12 @@ static void execute(ToxWindow *self, char *u_cmd)
     leading_spc++;
   memmove(cmd, cmd + leading_spc, 256 - leading_spc);
 
+  int cmd_end = strlen(cmd);
+  while (cmd_end > 0 && cmd_end--)
+    if (!isspace(cmd[cmd_end]))
+      break;
+  cmd[cmd_end + 1] = '\0';
+
   if (!strcmp(cmd, "quit") || !strcmp(cmd, "exit") || !strcmp(cmd, "q")) {
     endwin();
     exit(0);
