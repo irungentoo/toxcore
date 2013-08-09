a7098f04a431e7cf52c9a0308de878b4c8ea5254
Red--Dot <eniz_vukovic@hotmail.com>
2013-08-09 16:10:16 +0200

    Stared adding media session

---
+++ testing/cmake/nToxAudio.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION 2.6.0)
+project(nTox C)
+
+set(exe_name nTox)
+
+add_executable(${exe_name}
+	nTox.c misc_tools.c)
+
+target_link_libraries(${exe_name}
+	ncurses)
+
+linkCoreLibraries(${exe_name})
