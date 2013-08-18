ebe4a3f8d01d6eaed305aa36054725739961d9e6
Sean Qureshi <stqism@risingstormgames.com>
2013-08-17 19:43:10 -0700

    Added more colors

--- testing/toxic/main.c
+++ testing/toxic/main.c
@@ -52,6 +52,10 @@ static void init_term()
         init_pair(3, COLOR_RED, COLOR_BLACK);
         init_pair(4, COLOR_BLUE, COLOR_BLACK);
         init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(7, COLOR_BLACK, COLOR_BLACK);
+        init_pair(8, COLOR_BLACK, COLOR_WHITE);
+
     }
 
     refresh();
