527943546b76c095ac1f8331adfe47f3d15d3bc5
Red--Dot <eniz_vukovic@hotmail.com>
2013-08-24 01:58:48 +0200

    Added error handlers

---
+++ Tuxrtp/rtp_error.c
@@ -0,0 +1,57 @@
+#include "rtp_error.h"
+#include "rtp_helper.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct rtp_error_s{
+    char* _message;
+    int   _id;
+
+} rtp_error_t;
+
+static rtp_error_t* _register = NULL;
+static size_t _it = 0;
+
+void t_rtperr_register( int _id, const char* _info )
+{
+    size_t _info_size = strlen(_info);
+
+    if ( !_register ){
+        _register = malloc( sizeof *_register);
+    }
+    else {
+        _register = realloc( _register, sizeof (rtp_error_t) * (_it + 1) );
+    }
+
+    rtp_error_t* _current = & _register[_it];
+
+    _current->_id = _id;
+    _current->_message = malloc( _info_size );
+    t_memcpy(_current->_message, _info, _info_size);
+    _it ++;
+}
+
+const char* t_rtperr(int _errno)
+{
+    if ( !_register )
+        return "Unregistered";
+
+    uint32_t _i;
+
+    for ( _i = _it; _i--; ){
+        if ( _register[_i]._id == _errno ){
+            return _register[_i]._message;
+        }
+    }
+
+    return "Invalid error id!";
+}
+
+void t_rtperr_print(const char* _val, ...)
+{
+    va_list _args;
+    va_start(_args, _val);
+    vfprintf(stderr, _val, _args);
+    va_end(_args);
+}
---
+++ Tuxrtp/rtp_error.h
@@ -0,0 +1,20 @@
+#ifndef _RTP_ERROR_
+#define _RTP_ERROR_
+
+#define PRINT_FORMAT "Error %d: %s at %s:%d\n"
+#define PRINT_ARGS( _errno ) _errno, t_rtperr(_errno), __FILE__, __LINE__
+
+
+const char* t_rtperr(int _errno);
+void        t_rtperr_register( int _id, const char* _info );
+
+void        t_invoke_error( int _id );
+void        t_rtperr_print(const char* _val, ...);
+
+#define t_perror( _errno ) t_rtperr_print ( PRINT_FORMAT, PRINT_ARGS ( _errno ) )
+
+#ifdef _STDIO_H /* I don't want you be telling me error handlers produce errors */
+#define t_errexit( _errno ) exit(-_errno)
+#endif /* _STDIO_H */
+
+#endif /* _RTP_ERROR_ */
