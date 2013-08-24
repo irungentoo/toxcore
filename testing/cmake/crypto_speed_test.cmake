1ec70337cfbf42f5f10d20c6152cdc5cf635df1b
jin-eld <jin at mediatomb dot cc>
2013-08-24 03:36:43 +0300

    Remove cmake build system

--- testing/cmake/crypto_speed_test.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 2.6.0)
-project(crypto_speed_test C)
-
-set(exe_name crypto_speed_test)
-
-add_executable(${exe_name}
-	crypto_speed_test.c)
-
-linkCoreLibraries(${exe_name})
