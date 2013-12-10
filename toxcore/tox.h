2d841fb7917a8970de176e490613ff3e77abd47d
AZ Huang <aitjcize@gmail.com>
2013-12-10 17:54:04 +0800

    More API consistent fix.

--- toxcore/tox.h
+++ toxcore/tox.h
@@ -303,7 +303,7 @@ void tox_callback_friend_message(Tox *tox, void (*function)(Tox *tox, int, uint8
 /* Set the function that will be executed when an action from a friend is received.
  *  Function format is: function(int friendnumber, uint8_t * action, uint32_t length)
  */
-void tox_callback_action(Tox *tox, void (*function)(Tox *tox, int, uint8_t *, uint16_t, void *), void *userdata);
+void tox_callback_friend_action(Tox *tox, void (*function)(Tox *tox, int, uint8_t *, uint16_t, void *), void *userdata);
 
 /* Set the callback for name changes.
  *  function(int friendnumber, uint8_t *newname, uint16_t length)
