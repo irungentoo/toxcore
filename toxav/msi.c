285c830e4e8dc378457865944bf6afe1ebbfd3e7
mannol <eniz_vukovic@hotmail.com>
2014-02-15 22:36:15 +0100

    Moved event to toxav

--- toxav/msi.c
+++ toxav/msi.c
@@ -18,8 +18,7 @@
  *   along with Tox. If not, see <http://www.gnu.org/licenses/>.
  *
  * 
- *   Report bugs/suggestions to me ( mannol ) at either #tox-dev @ freenode.net:6667 or
- *   my email: eniz_vukovic@hotmail.com
+ *   Report bugs/suggestions at #tox-dev @ freenode.net:6667
  */
 
 
@@ -30,9 +29,9 @@
 #define _BSD_SOURCE
 
 #include "msi.h"
+#include "event.h"
 #include "../toxcore/util.h"
 #include "../toxcore/network.h"
-#include "../toxcore/event.h"
 #include "../toxcore/Messenger.h"
 
 #include <assert.h>
