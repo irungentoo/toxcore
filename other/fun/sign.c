0b5017ca822e65055e18b1bed7741af88a5dc855
Sean <sean@tox.im>
2014-02-15 20:01:18 -0800

    Add GPL header

--- other/fun/sign.c
+++ other/fun/sign.c
@@ -1,4 +1,22 @@
 /* Binary signer/checker using ed25519
+
+ *
+ *  Copyright (C) 2013 Tox project All Rights Reserved.
+ *
+ *  This file is part of Tox.
+ *
+ *  Tox is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tox.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Compile with:
  *  gcc -o sign sign.c -lsodium
