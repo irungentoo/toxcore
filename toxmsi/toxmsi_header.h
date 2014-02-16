59100c37639717d9e9306d92a275fbfab82d415c
Sean <sean@tox.im>
2014-02-15 19:45:52 -0800

    Added GPL header

--- toxmsi/toxmsi_header.h
+++ toxmsi/toxmsi_header.h
@@ -1,3 +1,26 @@
+/* toxmsi_header.h
+ *
+ *  Copyright (C) 2013 Tox project All Rights Reserved.
+ *
+ *  This file is part of Tox.
+ *
+ *  Tox is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*----------------------------------------------------------------------------------*/
+
 #ifndef _MSI_HEADER_
 #define _MSI_HEADER_
 
