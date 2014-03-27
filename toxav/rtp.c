irungentoo/toxcore

commit 6a78e2e71c1471b89646a71a1adeb0529cfb73d4

Fixed several bugs and added some features

toxav/rtp.c
@@ -262,17 +262,8 @@ RTPHeader *extract_header ( const uint8_t *payload, int length )
         return NULL;
     }
 
-    if ( _cc > 0 ) {
-        _retu->csrc = calloc (_cc, sizeof (uint32_t));
-        assert(_retu->csrc);
-
-    } else { /* But this should not happen ever */
-        /* Deallocate */
-        free(_retu);
-        return NULL;
-    }
-
-
+    memset(_retu->csrc, 0, 16);
+    
     _retu->marker_payloadt = *_it;
     ++_it;
     _retu->length = _length;
@@ -362,13 +353,11 @@ uint8_t *add_header ( RTPHeader *header, uint8_t *payload )
     _it += 4;
     U32_to_bytes( _it, header->ssrc);
 
-    if ( header->csrc ) {
-        uint8_t _x;
+    uint8_t _x;
 
-        for ( _x = 0; _x < _cc; _x++ ) {
-            _it += 4;
-            U32_to_bytes( _it, header->csrc[_x]);
-        }
+    for ( _x = 0; _x < _cc; _x++ ) {
+        _it += 4;
+        U32_to_bytes( _it, header->csrc[_x]);
     }
 
     return _it + 4;
@@ -424,19 +413,11 @@ RTPHeader *build_header ( RTPSession *session )
     _retu->timestamp = ((uint32_t)(current_time() / 1000)); /* micro to milli */
     _retu->ssrc = session->ssrc;
 
-    if ( session->cc > 0 ) {
-        _retu->csrc = calloc(session->cc, sizeof (uint32_t));
-        assert(_retu->csrc);
+    int i;
 
-        int i;
-
-        for ( i = 0; i < session->cc; i++ ) {
-            _retu->csrc[i] = session->csrc[i];
-        }
-    } else {
-        _retu->csrc = NULL;
-    }
-
+    for ( i = 0; i < session->cc; i++ ) 
+        _retu->csrc[i] = session->csrc[i];
+        
     _retu->length = 12 /* Minimum header len */ + ( session->cc * size_32 );
 
     return _retu;
@@ -480,9 +461,7 @@ RTPMessage *msg_parse ( uint16_t sequnum, const uint8_t *data, int length )
             _retu->length -= ( 4 /* Minimum ext header len */ + _retu->ext_header->length * size_32 );
             _from_pos += ( 4 /* Minimum ext header len */ + _retu->ext_header->length * size_32 );
         } else { /* Error */
-            free (_retu->ext_header);
-            free (_retu->header);
-            free (_retu);
+            rtp_free_msg(NULL, _retu);
             return NULL;
         }
     } else {
@@ -545,7 +524,7 @@ int rtp_handle_packet ( void *object, IP_Port ip_port, uint8_t *data, uint32_t l
             _decrypted_length = decrypt_data_symmetric(
                                     (uint8_t *)_session->decrypt_key, _session->nonce_cycle, data + 3, length - 3, _plain );
 
-            if ( !_decrypted_length ) return -1; /* This packet is not encrypted properly */
+            if ( _decrypted_length == -1 ) return -1; /* This packet is not encrypted properly */
 
             /* Otherwise, if decryption is ok with new cycle, set new cycle
              */
@@ -554,7 +533,7 @@ int rtp_handle_packet ( void *object, IP_Port ip_port, uint8_t *data, uint32_t l
             _decrypted_length = decrypt_data_symmetric(
                                     (uint8_t *)_session->decrypt_key, _calculated, data + 3, length - 3, _plain );
 
-            if ( !_decrypted_length ) return -1; /* This is just an error */
+            if ( _decrypted_length == -1 ) return -1; /* This is just an error */
 
             /* A new cycle setting. */
             memcpy(_session->nonce_cycle, _session->decrypt_nonce, crypto_secretbox_NONCEBYTES);
@@ -801,16 +780,11 @@ int rtp_send_msg ( RTPSession *session, Messenger *messenger, const uint8_t *dat
 void rtp_free_msg ( RTPSession *session, RTPMessage *msg )
 {
     if ( !session ) {
-        free ( msg->header->csrc );
-
         if ( msg->ext_header ) {
             free ( msg->ext_header->table );
             free ( msg->ext_header );
         }
     } else {
-        if ( session->csrc != msg->header->csrc )
-            free ( msg->header->csrc );
-
         if ( msg->ext_header && session->ext_header != msg->ext_header ) {
             free ( msg->ext_header->table );
             free ( msg->ext_header );
@@ -917,15 +891,21 @@ int rtp_terminate_session ( RTPSession *session, Messenger *messenger )
 {
     if ( !session )
         return -1;
-
+        
     custom_user_packet_registerhandler(messenger, session->dest, session->prefix, NULL, NULL);
-
+    
+    rtp_release_session_recv(session);
+    
+    pthread_mutex_lock(&session->mutex);
+    
     free ( session->ext_header );
     free ( session->csrc );
     free ( session->decrypt_nonce );
     free ( session->encrypt_nonce );
     free ( session->nonce_cycle );
 
+    pthread_mutex_unlock(&session->mutex);
+    
     pthread_mutex_destroy(&session->mutex);
 
     /* And finally free session */

configure.ac
@@ -487,7 +487,8 @@ fi
 if test "x$BUILD_AV" = "xyes"; then
     # toxcore lib needs an global?
     # So far this works okay
-    AV_LIBS="$OPUS_LIBS $VPX_LIBS"
+    ## What about pthread?
+    AV_LIBS="$OPUS_LIBS $VPX_LIBS -pthread"
     AC_SUBST(AV_LIBS)
 
     AV_CFLAGS="$OPUS_CFLAGS $VPX_CFLAGS"

libtoxav.pc
@@ -1,0 +1,11 @@
+prefix=/usr/local
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+
+Name: libtoxav
+Description: Tox A/V library
+Requires:
+Version: 0.0.0
+Libs: -L${libdir} -ltoxav -lopus  -lvpx -lm  -pthread
+Cflags: -I${includedir}

toxav/media.c
@@ -46,18 +46,28 @@ struct jitter_buffer {
     uint8_t id_set;
 };
 
+int empty_queue(struct jitter_buffer *q)
+{
+    while (q->size > 0) {
+        rtp_free_msg(NULL, q->queue[q->front]);
+        q->front++;
+        
+        if (q->front == q->capacity)
+            q->front = 0;
+        
+        q->size--;
+    }
+    
+    q->id_set = 0;
+    q->queue_ready = 0;
+    return 0;
+}
 
 struct jitter_buffer *create_queue(int capacity)
 {
     struct jitter_buffer *q;
-    q = (struct jitter_buffer *)calloc(sizeof(struct jitter_buffer), 1);
-    q->queue = (RTPMessage **)calloc(sizeof(RTPMessage *), capacity);
-    int i = 0;
-
-    for (i = 0; i < capacity; ++i) {
-        q->queue[i] = NULL;
-    }
-
+    q = calloc(sizeof(struct jitter_buffer), 1);
+    q->queue = calloc(sizeof(RTPMessage *), capacity);
     q->size = 0;
     q->capacity = capacity;
     q->front = 0;
@@ -81,7 +91,7 @@ uint8_t sequence_number_older(uint16_t sn_a, uint16_t sn_b, uint32_t ts_a, uint3
 /* success is 0 when there is nothing to dequeue, 1 when there's a good packet, 2 when there's a lost packet */
 RTPMessage *dequeue(struct jitter_buffer *q, int *success)
 {
-    if (q->size == 0 || q->queue_ready == 0) {
+    if (q->size == 0 || q->queue_ready == 0) { /* Empty queue */
         q->queue_ready = 0;
         *success = 0;
         return NULL;
@@ -103,13 +113,13 @@ RTPMessage *dequeue(struct jitter_buffer *q, int *success)
             q->current_ts = next_ts;
         } else {
             if (sequence_number_older(next_id, q->current_id, next_ts, q->current_ts)) {
-                printf("nextid: %d current: %d\n", next_id, q->current_id);
+                /*printf("nextid: %d current: %d\n", next_id, q->current_id);*/
                 q->current_id = (q->current_id + 1) % MAX_SEQU_NUM;
                 *success = 2; /* tell the decoder the packet is lost */
                 return NULL;
             } else {
                 /* packet too old */
-                printf("packet too old\n");
+                /*printf("packet too old\n");*/
                 *success = 0;
                 return NULL;
             }
@@ -128,27 +138,12 @@ RTPMessage *dequeue(struct jitter_buffer *q, int *success)
     return q->queue[front];
 }
 
-int empty_queue(struct jitter_buffer *q)
-{
-    while (q->size > 0) {
-        q->size--;
-        rtp_free_msg(NULL, q->queue[q->front]);
-        q->front++;
-
-        if (q->front == q->capacity)
-            q->front = 0;
-    }
-
-    q->id_set = 0;
-    q->queue_ready = 0;
-    return 0;
-}
 
 int queue(struct jitter_buffer *q, RTPMessage *pk)
 {
-    if (q->size == q->capacity) {
-        printf("buffer full, emptying buffer...\n");
+    if (q->size == q->capacity) { /* Full, empty queue */
         empty_queue(q);
+        /*rtp_free_msg(NULL, pk);*/
         return 0;
     }
 
@@ -180,7 +175,7 @@ int queue(struct jitter_buffer *q, RTPMessage *pk)
             temp = q->queue[a];
             q->queue[a] = q->queue[b];
             q->queue[b] = temp;
-            printf("had to swap\n");
+            /*printf("had to swap\n");*/
         } else {
             break;
         }
@@ -229,7 +224,7 @@ int init_video_encoder(CodecState *cs, uint16_t width, uint16_t height, uint32_t
     int res = vpx_codec_enc_config_default(VIDEO_CODEC_ENCODER_INTERFACE, &cfg, 0);
 
     if (res) {
-        printf("Failed to get config: %s\n", vpx_codec_err_to_string(res));
+        fprintf(stderr, "Failed to get config: %s\n", vpx_codec_err_to_string(res));
         return -1;
     }
 
@@ -266,49 +261,43 @@ CodecState *codec_init_session ( uint32_t audio_bitrate,
                                  uint16_t video_height,
                                  uint32_t video_bitrate )
 {
-    CodecState *_retu = calloc(sizeof(CodecState), 1);
-    assert(_retu);
+    CodecState *retu = calloc(sizeof(CodecState), 1);
+    assert(retu);
 
-    _retu->audio_bitrate = audio_bitrate;
-    _retu->audio_sample_rate = audio_sample_rate;
+    retu->audio_bitrate = audio_bitrate;
+    retu->audio_sample_rate = audio_sample_rate;
 
     /* Encoders */
-    if (!video_width || !video_height) {
-        video_width = 320;
-        video_height = 240;
+    if (!video_width || !video_height) { /* Disable video */
+        /*video_width = 320;
+        video_height = 240; */
+    }
+    else {
+        retu->supported_actions |= ( 0 == init_video_encoder(retu, video_width, video_height, video_bitrate) ) ? v_encoding : 0;
+        retu->supported_actions |= ( 0 == init_video_decoder(retu) ) ? v_decoding : 0;
     }
 
-    if ( 0 == init_video_encoder(_retu, video_width, video_height, video_bitrate) )
-        printf("Video encoder initialized!\n");
+    retu->supported_actions |= ( 0 == init_audio_encoder(retu, audio_channels) ) ? a_encoding : 0;
+    retu->supported_actions |= ( 0 == init_audio_decoder(retu, audio_channels) ) ? a_decoding : 0;
 
-    if ( 0 == init_audio_encoder(_retu, audio_channels) )
-        printf("Audio encoder initialized!\n");
-
-
-    /* Decoders */
-    if ( 0 == init_video_decoder(_retu) )
-        printf("Video decoder initialized!\n");
-
-    if ( 0 == init_audio_decoder(_retu, audio_channels) )
-        printf("Audio decoder initialized!\n");
-
-
-    return _retu;
+    return retu;
 }
 
 void codec_terminate_session ( CodecState *cs )
 {
-    if ( cs->audio_encoder ) {
+    if ( cs->audio_encoder ) 
         opus_encoder_destroy(cs->audio_encoder);
-        printf("Terminated encoder!\n");
-    }
-
-    if ( cs->audio_decoder ) {
+    
+    if ( cs->audio_decoder ) 
         opus_decoder_destroy(cs->audio_decoder);
-        printf("Terminated decoder!\n");
-    }
+    
 
-    /* TODO: Terminate video */
-    vpx_codec_destroy(&cs->v_decoder);
-    vpx_codec_destroy(&cs->v_encoder);
+    /* TODO: Terminate video 
+     *           Do what???
+     */
+    if ( cs->supported_actions & v_decoding )
+        vpx_codec_destroy(&cs->v_decoder);
+    
+    if ( cs->supported_actions & v_encoding )
+        vpx_codec_destroy(&cs->v_encoder);
 }

toxav/media.h
@@ -38,6 +38,14 @@
 /* Audio encoding/decoding */
 #include <opus/opus.h>
 
+enum _actions
+{
+    no_actions,
+    a_encoding = 1 << 0,
+    a_decoding = 1 << 1,
+    v_encoding = 1 << 2,
+    v_decoding = 1 << 3
+};
 
 typedef struct _CodecState {
 
@@ -56,12 +64,13 @@ typedef struct _CodecState {
     /* audio decoding */
     OpusDecoder *audio_decoder;
 
+    uint64_t supported_actions; /* Encoding decoding etc */
+    
 } CodecState;
 
 typedef struct _RTPMessage RTPMessage;
 
 struct jitter_buffer *create_queue(int capacity);
-int empty_queue(struct jitter_buffer *q);
 
 int queue(struct jitter_buffer *q, RTPMessage *pk);
 RTPMessage *dequeue(struct jitter_buffer *q, int *success);

toxav/msi.c
@@ -616,10 +616,15 @@ int send_message ( MSISession *session, MSIMessage *msg, uint32_t to )
 void flush_peer_type ( MSISession *session, MSIMessage *msg, int peer_id )
 {
     if ( msg->calltype.header_value ) {
-        if ( strcmp ( ( const char * ) msg->calltype.header_value, CT_AUDIO_HEADER_VALUE ) == 0 ) {
+        uint8_t hdrval [MSI_MAXMSG_SIZE]; /* Make sure no overflow */
+        
+        memcpy(hdrval, msg->calltype.header_value, msg->calltype.size);
+        hdrval[msg->calltype.size] = '\0';
+        
+        if ( strcmp ( ( const char * ) hdrval, CT_AUDIO_HEADER_VALUE ) == 0 ) {
             session->call->type_peer[peer_id] = type_audio;
 
-        } else if ( strcmp ( ( const char * ) msg->calltype.header_value, CT_VIDEO_HEADER_VALUE ) == 0 ) {
+        } else if ( strcmp ( ( const char * ) hdrval, CT_VIDEO_HEADER_VALUE ) == 0 ) {
             session->call->type_peer[peer_id] = type_video;
         } else {} /* Error */
     } else {} /* Error */
@@ -670,7 +675,7 @@ int handle_error ( MSISession *session, MSICallError errid, uint32_t to )
     session->last_error_id = errid;
     session->last_error_str = stringify_error ( errid );
 
-    event.rise ( callbacks[MSI_OnError], session->agent_handler );
+    if ( callbacks[MSI_OnError] ) event.rise ( callbacks[MSI_OnError], session->agent_handler );
 
     return 0;
 }
@@ -723,12 +728,12 @@ void *handle_timeout ( void *arg )
         uint16_t _it = 0;
 
         for ( ; _it < _peer_count; _it++ )
-            msi_cancel ( arg, _peers[_it], (const uint8_t *)"Timeout" );
+            msi_cancel ( arg, _peers[_it], "Timeout" );
 
     }
 
-    ( *callbacks[MSI_OnRequestTimeout] ) ( _session->agent_handler );
-    ( *callbacks[MSI_OnEnding ] )        ( _session->agent_handler );
+    if ( callbacks[MSI_OnRequestTimeout] ) callbacks[MSI_OnRequestTimeout] ( _session->agent_handler );
+    if ( callbacks[MSI_OnEnding] ) callbacks[MSI_OnEnding ] ( _session->agent_handler );
 
     return NULL;
 }
@@ -864,7 +869,7 @@ int handle_recv_invite ( MSISession *session, MSIMessage *msg )
     send_message ( session, _msg_ringing, msg->friend_id );
     free_message ( _msg_ringing );
 
-    event.rise ( callbacks[MSI_OnInvite], session->agent_handler );
+    if ( callbacks[MSI_OnInvite] ) event.rise ( callbacks[MSI_OnInvite], session->agent_handler );
 
     return 1;
 }
@@ -888,7 +893,7 @@ int handle_recv_start ( MSISession *session, MSIMessage *msg )
 
     flush_peer_type ( session, msg, 0 );
 
-    event.rise ( callbacks[MSI_OnStart], session->agent_handler );
+    if ( callbacks[MSI_OnStart] ) event.rise ( callbacks[MSI_OnStart], session->agent_handler );
 
     return 1;
 }
@@ -905,7 +910,7 @@ int handle_recv_reject ( MSISession *session, MSIMessage *msg )
     free_message ( _msg_end );
 
     event.timer_release ( session->call->request_timer_id );
-    event.rise ( callbacks[MSI_OnReject], session->agent_handler );
+    if ( callbacks[MSI_OnReject] ) event.rise ( callbacks[MSI_OnReject], session->agent_handler );
     session->call->request_timer_id = event.timer_alloc ( handle_timeout, session, m_deftout );
 
     return 1;
@@ -920,7 +925,7 @@ int handle_recv_cancel ( MSISession *session, MSIMessage *msg )
 
     terminate_call ( session );
 
-    event.rise ( callbacks[MSI_OnCancel], session->agent_handler );
+    if ( callbacks[MSI_OnCancel] ) event.rise ( callbacks[MSI_OnCancel], session->agent_handler );
 
     return 1;
 }
@@ -938,7 +943,7 @@ int handle_recv_end ( MSISession *session, MSIMessage *msg )
 
     terminate_call ( session );
 
-    event.rise ( callbacks[MSI_OnEnd], session->agent_handler );
+    if ( callbacks[MSI_OnEnd] ) event.rise ( callbacks[MSI_OnEnd], session->agent_handler );
 
     return 1;
 }
@@ -952,7 +957,7 @@ int handle_recv_ringing ( MSISession *session, MSIMessage *msg )
         return 0;
 
     session->call->ringing_timer_id = event.timer_alloc ( handle_timeout, session, session->call->ringing_tout_ms );
-    event.rise ( callbacks[MSI_OnRinging], session->agent_handler );
+    if ( callbacks[MSI_OnRinging] ) event.rise ( callbacks[MSI_OnRinging], session->agent_handler );
 
     return 1;
 }
@@ -991,7 +996,7 @@ int handle_recv_starting ( MSISession *session, MSIMessage *msg )
 
     flush_peer_type ( session, msg, 0 );
 
-    event.rise ( callbacks[MSI_OnStarting], session->agent_handler );
+    if ( callbacks[MSI_OnStarting] ) event.rise ( callbacks[MSI_OnStarting], session->agent_handler );
     event.timer_release ( session->call->ringing_timer_id );
 
     return 1;
@@ -999,14 +1004,22 @@ int handle_recv_starting ( MSISession *session, MSIMessage *msg )
 int handle_recv_ending ( MSISession *session, MSIMessage *msg )
 {
     assert ( session );
-
+    
     if ( has_call_error ( session, msg ) == 0 )
         return 0;
-
-
+    
+    /* Do the callback before ending 
+    if ( callbacks[MSI_OnEnding] ) event.rise ( callbacks[MSI_OnEnding], session->agent_handler );
+    */
+    
+    /* Stop timer */
+    event.timer_release ( session->call->request_timer_id );
+    
+    /* Call callback */
+    if ( callbacks[MSI_OnEnding] ) callbacks[MSI_OnEnding](session->agent_handler);
+    
+    /* Terminate call */
     terminate_call ( session );
-
-    event.rise ( callbacks[MSI_OnEnding], session->agent_handler );
 
     return 1;
 }
@@ -1023,7 +1036,7 @@ int handle_recv_error ( MSISession *session, MSIMessage *msg )
 
     terminate_call ( session );
 
-    event.rise ( callbacks[MSI_OnEnding], session->agent_handler );
+    if ( callbacks[MSI_OnEnding] ) event.rise ( callbacks[MSI_OnEnding], session->agent_handler );
 
     return 1;
 }
@@ -1081,8 +1094,13 @@ void msi_handle_packet ( Messenger *messenger, int source, uint8_t *data, uint16
     /* Now handle message */
 
     if ( _msg->request.header_value ) { /* Handle request */
-
-        const uint8_t *_request_value = _msg->request.header_value;
+        
+        if ( _msg->response.size > 32 ) goto free_end;
+        
+        uint8_t _request_value[32];
+        
+        memcpy(_request_value, _msg->request.header_value, _msg->request.size);
+        _request_value[_msg->request.size] = '\0';
 
         if ( same ( _request_value, stringify_request ( invite ) ) ) {
             handle_recv_invite ( _session, _msg );
@@ -1100,15 +1118,17 @@ void msi_handle_packet ( Messenger *messenger, int source, uint8_t *data, uint16
             handle_recv_end ( _session, _msg );
         }
 
-        else {
-            free_message ( _msg );
-            return;
-        }
+        else goto free_end;
 
     } else if ( _msg->response.header_value ) { /* Handle response */
-
-        const uint8_t *_response_value = _msg->response.header_value;
-
+        
+        if ( _msg->response.size > 32 ) goto free_end;
+        
+        uint8_t _response_value[32];
+        
+        memcpy(_response_value, _msg->response.header_value, _msg->response.size);
+        _response_value[_msg->response.size] = '\0';
+        
         if ( same ( _response_value, stringify_response ( ringing ) ) ) {
             handle_recv_ringing ( _session, _msg );
 
@@ -1120,10 +1140,8 @@ void msi_handle_packet ( Messenger *messenger, int source, uint8_t *data, uint16
 
         } else if ( same ( _response_value, stringify_response ( error ) ) ) {
             handle_recv_error ( _session, _msg );
-        } else {
-            free_message ( _msg );
-            return;
-        }
+            
+        } else goto free_end;
 
         /* Got response so cancel timer */
         if ( _session->call )
@@ -1131,7 +1149,7 @@ void msi_handle_packet ( Messenger *messenger, int source, uint8_t *data, uint16
 
     }
 
-    free_message ( _msg );
+    free_end:free_message ( _msg );
 }
 
 
@@ -1286,19 +1304,19 @@ int msi_hangup ( MSISession *session )
     if ( !session->call || session->call->state != call_active )
         return -1;
 
-    MSIMessage *_msg_ending = msi_new_message ( TYPE_REQUEST, stringify_request ( end ) );
+    MSIMessage *_msg_end = msi_new_message ( TYPE_REQUEST, stringify_request ( end ) );
 
     /* hangup for each peer */
     int _it = 0;
+    
+    for ( ; _it < session->call->peer_count; _it ++ ) 
+        send_message ( session, _msg_end, session->call->peers[_it] );
+    
 
-    for ( ; _it < session->call->peer_count; _it ++ )
-        send_message ( session, _msg_ending, session->call->peers[_it] );
-
-
-    free_message ( _msg_ending );
+    free_message ( _msg_end );
 
     session->call->request_timer_id = event.timer_alloc ( handle_timeout, session, m_deftout );
-
+    
     return 0;
 }
 
@@ -1352,13 +1370,13 @@ int msi_answer ( MSISession *session, MSICallType call_type )
  * @param reason Set optional reason header. Pass NULL if none.
  * @return int
  */
-int msi_cancel ( MSISession *session, uint32_t peer, const uint8_t *reason )
+int msi_cancel ( MSISession *session, uint32_t peer, const char *reason )
 {
     assert ( session );
 
     MSIMessage *_msg_cancel = msi_new_message ( TYPE_REQUEST, stringify_request ( cancel ) );
 
-    if ( reason ) msi_msg_set_reason(_msg_cancel, reason, strlen((const char *)reason));
+    if ( reason ) msi_msg_set_reason(_msg_cancel, (const uint8_t*)reason, strlen(reason));
 
     send_message ( session, _msg_cancel, peer );
     free_message ( _msg_cancel );

toxav/msi.h
@@ -206,7 +206,7 @@ int msi_answer ( MSISession *session, MSICallType call_type );
  * @param reason Set optional reason header. Pass NULL if none.
  * @return int
  */
-int msi_cancel ( MSISession *session, uint32_t peer, const uint8_t *reason );
+int msi_cancel ( MSISession* session, uint32_t peer, const char* reason );
 
 
 /**

commit 5c49f7d2b9371ac98bd38016ea61f36f4b93b20e

fix RTPHeader filling by zeros underflow
regression from 6a78e2e71c1471b89646a71a1adeb0529cfb73d4

toxav/rtp.c
@@ -262,7 +262,7 @@ RTPHeader *extract_header ( const uint8_t *payload, int length )
         return NULL;
     }
 
-    memset(_retu->csrc, 0, 16);
+    memset(_retu->csrc, 0, 16 * sizeof (uint32_t));
     
     _retu->marker_payloadt = *_it;
     ++_it;