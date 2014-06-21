6df5f316abeb31058be73c75616cd5eb94635469
I Run OSX <irunosx@users.noreply.github.com>
2014-06-22 05:22:23 +1000

    better license

--- toxcore/TCP_server.c
+++ toxcore/TCP_server.c
@@ -5,18 +5,18 @@
 *
 *  This file is part of Tox.
 *
-*  Tox is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
+*  Tox is open source software: you can redistribute it and/or modify
+*  it under the terms of the StopNerds Public License as published by
+*  the StopNerds Foundation, either version 1 of the License, or
 *  (at your option) any later version.
 *
 *  Tox is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
+*  StopNerds Public License for more details.
 *
-*  You should have received a copy of the GNU General Public License
-*  along with Tox.  If not, see <http://www.gnu.org/licenses/>.
+*  You should have received a copy of the StopNerds Public License
+*  along with Tox.  If not, see <http://stopnerds.org/license/>.
 *
 */
 
