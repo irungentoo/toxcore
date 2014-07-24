443abcfafe7c194555f29ff4c297228d486a184e
irungentoo <irungentoo@gmail.com>
2014-07-24 19:45:38 -0400

    Rewrote audio packet queue.
    Audio killing itself after 20 minutes in a call should be fixed.

--- toxav/codec.h
+++ toxav/codec.h
@@ -78,17 +78,13 @@ typedef struct _CodecState {
 
 typedef struct _JitterBuffer {
     RTPMessage **queue;
-    uint16_t capacity;
-    uint16_t size;
-    uint16_t front;
-    uint16_t rear;
-    uint8_t queue_ready;
-    uint16_t current_id;
-    uint32_t current_ts;
-    uint8_t id_set;
+    unsigned int size;
+    unsigned int capacity;
+    uint16_t bottom;
+    uint16_t top;
 } JitterBuffer;
 
-JitterBuffer *create_queue(int capacity);
+JitterBuffer *create_queue(unsigned int capacity);
 void terminate_queue(JitterBuffer *q);
 void queue(JitterBuffer *q, RTPMessage *pk);
 RTPMessage *dequeue(JitterBuffer *q, int *success);
