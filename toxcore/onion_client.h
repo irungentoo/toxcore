4858d2c6286d8a2cddf6a1f08f1d89a0dd5435ed
irungentoo <irungentoo@gmail.com>
2015-01-05 22:30:09 -0500

    Reduced timeout of onion paths on the first try by 1 second.

--- toxcore/onion_client.h
+++ toxcore/onion_client.h
@@ -40,7 +40,7 @@
 
 /* The timeout the first time the path is added and
    then for all the next consecutive times */
-#define ONION_PATH_FIRST_TIMEOUT 5
+#define ONION_PATH_FIRST_TIMEOUT 4
 #define ONION_PATH_TIMEOUT 10
 #define ONION_PATH_MAX_LIFETIME 1200
 #define ONION_PATH_MAX_NO_RESPONSE_USES 4
