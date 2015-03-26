722fefcdec42f6a887afa6dd9a59214d7e0988ab
Aleksey Mokhovikov <moxobukob@gmail.com>
2015-03-26 16:25:34 +0600

    Multiple nospam functionality is added. See documentation in tox.h for new api functions.

--- toxcore/friend_requests.h
+++ toxcore/friend_requests.h
@@ -25,12 +25,38 @@
 #define FRIEND_REQUESTS_H
 
 #include "friend_connection.h"
+#include <stdbool.h>
 
 #define MAX_FRIEND_REQUEST_DATA_SIZE (ONION_CLIENT_MAX_DATA_SIZE - (1 + sizeof(uint32_t)))
 
-typedef struct {
+/* Maximum nospam amount should be quite small to be effective against spam.
+ * Maximum allowed amount is given by MAX_NOSPAM_AMOUNT.
+ */
+
+#define MAX_NOSPAM_AMOUNT 16
+#define MAX_NOSPAM_DESCRIPTION_LENGTH 128
+#define NOSPAM_SIZE sizeof(uint32_t)
+#define NOSPAM_SPAM 0
+
+typedef enum NSERR {
+    NSERR_SUCCESS = 0,
+    NSERR_NOT_FOUND = 1,
+    NSERR_TOO_MANY = 2,
+    NSERR_ALREADY_EXISTS = 3,
+    NSERR_DESCRIPTION_TOO_LONG
+} NSERR;
+
+typedef struct No_Spam {
     uint32_t nospam;
-    void (*handle_friendrequest)(void *, const uint8_t *, const uint8_t *, size_t, void *);
+    uint8_t  description[MAX_NOSPAM_DESCRIPTION_LENGTH];
+    size_t description_length;
+} No_Spam;
+
+typedef struct {
+    No_Spam nospam[MAX_NOSPAM_AMOUNT];
+    uint32_t nospam_amount;
+
+    void (*handle_friendrequest)(void *, const uint8_t *, uint32_t, const uint8_t *, size_t, void *);
     uint8_t handle_friendrequest_isset;
     void *handle_friendrequest_object;
     void *handle_friendrequest_userdata;
@@ -47,9 +73,62 @@ typedef struct {
     uint16_t received_requests_index;
 } Friend_Requests;
 
-/* Set and get the nospam variable used to prevent one type of friend request spam. */
+/* Set and get the nospam variable used to prevent one type of friend request spam.
+ * This is old API.
+ */
 void set_nospam(Friend_Requests *fr, uint32_t num);
 uint32_t get_nospam(const Friend_Requests *fr);
+
+/*
+ * This function updates the nospam set. If num is in nospam set, then new_num will replace num.
+ * If new_num == NOSPAM_SPAM, which is 0, then num value will be removed from nospam set.
+ * If num == NOSPAM_SPAM, which is 0, then new_num will be added to nospam set.
+ * Passing num == 0 and new_num == 0 will return NSERR_SUCCESS and result no effect.
+ *
+ * Return: NSERR_SUCCESS on success, or NSERR_* on falure.
+ * If the call fails, the nospam set is unchanged.
+ */
+NSERR nospam_update(Friend_Requests *fr, uint32_t num, uint32_t new_num);
+
+/*
+ * Set description for num. Description in utf8 string.
+ * Passing 0 as descr_length will result no effect and NSERR_SUCCESS will be returned.
+ * Passing descr == 0 and descr_length != 0 will erase the description.
+ * Passing descr_length > MAX_NOSPAM_DESCRIPTION_LENGTH will result and error;
+ * If error happens then nothing is changed.
+ * On success description for nospam num is changed.
+ */
+NSERR nospam_descr_update(Friend_Requests *fr, uint32_t num, const uint8_t *descr, size_t descr_length);
+
+/*
+ * Returns nospam value and set nserr to NSERR_SUCCESS on success.
+ * Returns 0 and set nserr on failure.
+ */
+size_t nospam_descr_length(const Friend_Requests *fr, uint32_t num, NSERR *nserr);
+
+/*
+ * Copy description to descr for num.
+ * If no description exists, then descr is unchanged.
+ * returns NSERR_SUCCESS or NSERR_* on failure.
+ */
+NSERR nospam_descr(const Friend_Requests *fr, uint32_t num, uint8_t *descr);
+
+/*
+ * Returns the amount of nospams.
+ * Call can't fail.
+ */
+size_t nospam_count(const Friend_Requests *fr);
+
+/*
+ * Write to ns_list all nospams in nospam set.
+ * The size of ns_list is given with nospam_count function.
+ * Call can't fail.
+ */
+void nospam_list(const Friend_Requests *fr, uint32_t *ns_list);
+
+uint32_t nospam_saved_list_size(const Friend_Requests *fr);
+void nospam_list_save(const Friend_Requests *fr, uint8_t *data);
+void nospam_list_load(Friend_Requests *fr, const uint8_t *data, uint32_t size);
 
 /* Remove real_pk from received_requests list.
  *
@@ -61,7 +140,7 @@ int remove_request_received(Friend_Requests *fr, const uint8_t *real_pk);
 /* Set the function that will be executed when a friend request for us is received.
  *  Function format is function(uint8_t * public_key, uint8_t * data, size_t length, void * userdata)
  */
-void callback_friendrequest(Friend_Requests *fr, void (*function)(void *, const uint8_t *, const uint8_t *, size_t,
+void callback_friendrequest(Friend_Requests *fr, void (*function)(void *, const uint8_t *, uint32_t, const uint8_t *, size_t,
                             void *), void *object, void *userdata);
 
 /* Set the function used to check if a friend request should be displayed to the user or not.
