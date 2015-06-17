4ffd5584f9dcbd5ed321239c1d78a852fafdc0b8
Zetok Zalbavar <zexavexxe@gmail.com>
2015-06-17 10:38:53 +0100

    Improve documentation about tcp_port
    Apparently it's not entirely clear that it's not needed in clients.
    
    v2, as provided by @nurupo

--- other/apidsl/tox.in.h
+++ other/apidsl/tox.in.h
@@ -431,7 +431,15 @@ static class options {
     uint16_t end_port;
 
     /**
-     * The port to use for the TCP server. If 0, the tcp server is disabled.
+     * The port to use for the TCP server (relay). If 0, the TCP server is
+     * disabled.
+     *
+     * Enabling it is not required for Tox to function properly.
+     *
+     * When enabled, your Tox instance can act as a TCP relay for other Tox
+     * instance. This leads to increased traffic, thus when writing a client
+     * it is recommended to enable TCP server only if the user has an option
+     * to disable it.
      */
     uint16_t tcp_port;
 
