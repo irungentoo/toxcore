caebc5303aacaf1663767f0ebaddf16b6ea0cd1c
Michael Neumann <mneumann@ntecs.de>
2015-08-08 01:45:55 +0200

    Fix for DragonFlyBSD

--- testing/tox_shell.c
+++ testing/tox_shell.c
@@ -35,7 +35,7 @@
 
 #if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__)
 #include <util.h>
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__DragonFly__)
 #include <libutil.h>
 #else
 #include <pty.h>
