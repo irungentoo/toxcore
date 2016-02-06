7a24cfadb3b349839f1f77c02fb59c4e08cd21ba
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2016-02-06 13:44:00 -0800

    Added ablity to send keyframes lossless

--- toxav/rtp.c
+++ toxav/rtp.c
@@ -184,6 +184,85 @@ int rtp_send_data (RTPSession *session, const uint8_t *data, uint16_t length)
     return 0;
 }
 
+int rtp_send_lossless_data (RTPSession *session, const uint8_t *data, uint16_t length)
+{
+    if (!session) {
+        LOGGER_WARNING("No session!");
+        return -1;
+    }
+
+    uint8_t rdata[length + sizeof(struct RTPHeader) + 1];
+    memset(rdata, 0, sizeof(rdata));
+
+    rdata[0] = session->payload_type;
+
+    struct RTPHeader *header = (struct RTPHeader *)(rdata  + 1);
+
+    header->ve = 2;
+    header->pe = 0;
+    header->xe = 0;
+    header->cc = 0;
+
+    header->ma = 0;
+    header->pt = session->payload_type % 128;
+
+    header->sequnum = htons(session->sequnum);
+    header->timestamp = htonl(current_time_monotonic());
+    header->ssrc = htonl(session->ssrc);
+
+    header->cpart = 0;
+    header->tlen = htons(length);
+
+    if (MAX_CRYPTO_DATA_SIZE > length + sizeof(struct RTPHeader) + 1) {
+
+        /**
+         * The lenght is lesser than the maximum allowed lenght (including header)
+         * Send the packet in single piece.
+         */
+
+        memcpy(rdata + 1 + sizeof(struct RTPHeader), data, length);
+
+        if (-1 == send_custom_lossless_packet(session->m, session->friend_number, rdata, sizeof(rdata)))
+            LOGGER_WARNING("RTP send failed (len: %d)! std error: %s", sizeof(rdata), strerror(errno));
+    } else {
+
+        /**
+         * The lenght is greater than the maximum allowed lenght (including header)
+         * Send the packet in multiple pieces.
+         */
+
+        uint16_t sent = 0;
+        uint16_t piece = MAX_CRYPTO_DATA_SIZE - (sizeof(struct RTPHeader) + 1);
+
+        while ((length - sent) + sizeof(struct RTPHeader) + 1 > MAX_CRYPTO_DATA_SIZE) {
+            memcpy(rdata + 1 + sizeof(struct RTPHeader), data + sent, piece);
+
+            if (-1 == send_custom_lossless_packet(session->m, session->friend_number,
+                                               rdata, piece + sizeof(struct RTPHeader) + 1))
+                LOGGER_WARNING("RTP send failed (len: %d)! std error: %s",
+                               piece + sizeof(struct RTPHeader) + 1, strerror(errno));
+
+            sent += piece;
+            header->cpart = htons(sent);
+        }
+
+        /* Send remaining */
+        piece = length - sent;
+
+        if (piece) {
+            memcpy(rdata + 1 + sizeof(struct RTPHeader), data + sent, piece);
+
+            if (-1 == send_custom_lossless_packet(session->m, session->friend_number, rdata,
+                                               piece + sizeof(struct RTPHeader) + 1))
+                LOGGER_WARNING("RTP send failed (len: %d)! std error: %s",
+                               piece + sizeof(struct RTPHeader) + 1, strerror(errno));
+        }
+    }
+
+    session->sequnum ++;
+    return 0;
+}
+
 
 bool chloss (const RTPSession *session, const struct RTPHeader *header)
 {
