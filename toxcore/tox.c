4c7856b346b507132ab892a4a2590b1c1ffc0395
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2016-02-13 19:41:51 -0800

    Get SemVer more ready

--- toxcore/tox.c
+++ toxcore/tox.c
@@ -72,23 +72,25 @@ typedef struct Messenger Tox;
 
 uint32_t tox_version_major(void)
 {
-    return 0;
+    return TOX_VERSION_MAJOR;
 }
 
 uint32_t tox_version_minor(void)
 {
-    return 0;
+    return TOX_VERSION_MINOR;
 }
 
 uint32_t tox_version_patch(void)
 {
-    return 0;
+    return TOX_VERSION_PATCH;
 }
 
 bool tox_version_is_compatible(uint32_t major, uint32_t minor, uint32_t patch)
 {
-    //TODO
-    return 1;
+    if (major != TOX_VERSION_MAJOR)
+        return 0;
+    else
+        return 1;
 }
 
 
