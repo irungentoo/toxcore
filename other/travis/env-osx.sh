e59fd26d4987050e474a6de27dd503f32cc7820a
iphydf <iphydf@users.noreply.github.com>
2016-08-24 23:52:34 +0100

    Add OSX and Windows build to travis config.
    Tests for Windows are disabled for now, until we figure out which tests
    can successfully run on wine and select only those.

--- other/travis/env-osx.sh
+++ other/travis/env-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CMAKE=cmake
-NPROC=`nproc`
+NPROC=`sysctl -n hw.ncpu`
 CURDIR=$PWD
 
 RUN() {
---
+++ other/travis/env-windows.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CMAKE=$ARCH-w64-mingw32.shared-cmake
+NPROC=`nproc`
+CURDIR=/work
+
+RUN() {
+  ./dockcross "$@"
+}
+
+ENABLE_WINDOWS_TESTS=false
+
+TESTS() {
+  # Download Microsoft DLLs.
+  curl http://www.dlldump.com/dllfiles/I/iphlpapi.dll -o _build/iphlpapi.dll
+  curl http://www.dlldump.com/dllfiles/W/ws2_32.dll -o _build/ws2_32.dll
+
+  # Copy our dependency DLLs.
+  ./dockcross sh -c 'cp $WINEDLLPATH/*.dll _build'
+
+  # Run tests in docker.
+  if $ENABLE_WINDOWS_TESTS; then
+    ./dockcross "$@" || {
+      cat _build/Testing/Temporary/LastTest.log
+      false
+    }
+  fi
+}
