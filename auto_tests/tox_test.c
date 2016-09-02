a759ddc7ebe307e7a5ced4c89de2286c5675800d
iphydf <iphydf@users.noreply.github.com>
2016-09-02 13:56:55 +0100

    Re-enable group chat tests.
    They don't seem to be a lot less stable than the rest. Either way we regularly
    need to restart builds to make timeouts go away.

--- auto_tests/tox_test.c
+++ auto_tests/tox_test.c
@@ -1373,7 +1373,7 @@ Suite *tox_suite(void)
      * Anyone reading this is welcome to try to fix it, but because there is a
      * new version of group chats for Tox already completed, and nearly ready to
      * merge, No one is willing/available to give this test the time in needs */
-#ifndef TRAVIS_ENV
+#ifndef DISABLE_GROUP_TESTS
     DEFTESTCASE_SLOW(many_group, 80);
 #endif
 
