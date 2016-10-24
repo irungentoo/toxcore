de966cdf90843819e2f7287e22ddcb5f95491b18
iphydf <iphydf@users.noreply.github.com>
2016-10-24 16:06:09 +0100

    Error if format_test can't be executed.
    This ensures that on Travis, format_test will always be executed, or the
    build fails.

--- cmake/ApiDsl.cmake
+++ cmake/ApiDsl.cmake
@@ -10,14 +10,29 @@ find_program(APIDSL NAMES
   apidsl.byte
   ${CMAKE_SOURCE_DIR}/../apidsl/apigen.native)
 find_program(ASTYLE NAMES
-  astyle)
+  astyle
+  $ENV{ASTYLE})
 
 function(apidsl)
   if(APIDSL AND ASTYLE)
     foreach(in_file ${ARGN})
-      get_filename_component(dirname ${in_file} DIRECTORY)
+      # Get the directory component of the input file name.
+      if(CMAKE_VERSION VERSION_LESS 3.0)
+        execute_process(
+          COMMAND dirname ${in_file}
+          OUTPUT_VARIABLE dirname
+          OUTPUT_STRIP_TRAILING_WHITESPACE)
+      else()
+        get_filename_component(dirname ${in_file} DIRECTORY)
+      endif()
+
+      # Get the name without extension (i.e. without ".api.h").
       get_filename_component(filename ${in_file} NAME_WE)
+
+      # Put them together, with the new extension that is ".h".
       set(out_file ${CMAKE_SOURCE_DIR}/${dirname}/${filename}.h)
+
+      # Run apidsl.
       add_custom_command(
         OUTPUT ${out_file}
         COMMAND "${APIDSL}" "${CMAKE_SOURCE_DIR}/${in_file}"
