de623f9eb99acdc8522ae18c3abe9eea760371a4
iphydf <iphydf@users.noreply.github.com>
2016-12-18 21:30:05 +0000

    Add test to check if tox_new/tox_kill leaks.
    We create and destroy 20k tox instances and run a single tox_iterate on
    it. This test is not comprehensive, but provides a simple check to see
    whether the destruction properly cleans up memory and perhaps other
    resources.

---
+++ auto_tests/resource_leak_test.c
@@ -0,0 +1,50 @@
+#include "helpers.h"
+
+// See man 2 sbrk.
+#if _BSD_SOURCE || _SVID_SOURCE || \
+  (_XOPEN_SOURCE >= 500 || \
+   _XOPEN_SOURCE && _XOPEN_SOURCE_EXTENDED) && \
+  !(_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600)
+#define HAVE_SBRK 1
+#endif
+
+#if HAVE_SBRK
+#include <assert.h>
+#include <unistd.h>
+#endif
+
+#define ITERATIONS 20000
+
+int main(void)
+{
+    int i;
+
+    puts("Warming up: creating/deleting 10 tox instances");
+
+    // Warm-up.
+    for (i = 0; i < 10; i++) {
+        Tox *tox = tox_new(0, 0);
+        tox_iterate(tox, NULL);
+        tox_kill(tox);
+    }
+
+#if HAVE_SBRK
+    // Low water mark.
+    char *hwm = (char *)sbrk(0);
+#endif
+    printf("Creating/deleting %d tox instances\n", ITERATIONS);
+
+    for (i = 0; i < ITERATIONS; i++) {
+        Tox *tox = tox_new(0, 0);
+        tox_iterate(tox, NULL);
+        tox_kill(tox);
+#if HAVE_SBRK
+        char *next_hwm = (char *)sbrk(0);
+        assert(hwm == next_hwm);
+#endif
+    }
+
+    puts("Success: no resource leaks detected");
+
+    return 0;
+}
