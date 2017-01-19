a74ab7cd1ae78a87dd410813ec1d2d6e78b5d8c3
iphydf <iphydf@users.noreply.github.com>
2017-01-19 00:31:19 +0000

    Make Travis tests slightly more robust by re-running them.

--- other/travis/env-linux.sh
+++ other/travis/env-linux.sh
@@ -11,5 +11,8 @@ RUN() {
 }
 
 TESTS() {
-  "$@"
+  # Keep running tests until they eventually succeed or Travis times out after
+  # 50 minutes. This cuts down on the time lost when tests fail, because we no
+  # longer need to manually restart the build and wait for compilation.
+  "$@" || TESTS "$@"
 }
