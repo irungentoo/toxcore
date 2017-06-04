8479b82431697ae2d36bd068a9dd90b520abeb73
iphydf <iphydf@users.noreply.github.com>
2017-06-04 18:34:15 +0000

    Fold hstox tests into the general linux test.
    We can run the spectest from within cmake, so we should exercise that.

--- other/travis/env-linux.sh
+++ other/travis/env-linux.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export PATH=/opt/ghc/7.8.4/bin:/opt/cabal/1.18/bin:/opt/alex/3.1.7/bin:/opt/happy/1.19.5/bin:$PATH
+export PATH=$HOME/.cabal/bin:$PATH
+
 CMAKE=cmake
 CMAKE_EXTRA_FLAGS="$CMAKE_EXTRA_FLAGS -DFORMAT_TEST=ON"
 NPROC=`nproc`
