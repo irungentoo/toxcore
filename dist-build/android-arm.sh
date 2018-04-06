d134aecf3ef97009c0884fdfc1c178cf2b9a84b3
sudden6 <sudden6@gmx.at>
2018-04-06 11:43:59 +0200

    remove broken and unmaintained scripts

--- dist-build/android-arm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export CFLAGS="-Ofast -mthumb -marm -march=armv6"
-TARGET_ARCH=arm TOOLCHAIN_NAME=arm-linux-androideabi-4.8 HOST_COMPILER=arm-linux-androideabi "$(dirname "$0")/android-build.sh"
