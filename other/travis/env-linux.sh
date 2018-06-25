d22332dd505b5a269d379863a091d74522fce27b
iphydf <iphydf@users.noreply.github.com>
2018-06-25 15:13:15 +0000

    Remove the format test.
    It's annoying to have a test touch every source file. It causes a
    recompile of everything after every test run.

--- other/travis/env-linux.sh
+++ other/travis/env-linux.sh
@@ -4,7 +4,6 @@ export PATH=/opt/ghc/7.8.4/bin:/opt/cabal/1.18/bin:/opt/alex/3.1.7/bin:/opt/happ
 export PATH=$HOME/.cabal/bin:$PATH
 
 CMAKE=cmake
-CMAKE_EXTRA_FLAGS="$CMAKE_EXTRA_FLAGS -DFORMAT_TEST=ON"
 # Asan is disabled because it's currently broken on Travis.
 # See https://github.com/travis-ci/travis-ci/issues/9033.
 CMAKE_EXTRA_FLAGS="$CMAKE_EXTRA_FLAGS -DASAN=OFF"
