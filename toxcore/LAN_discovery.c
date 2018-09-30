ed9beef61c5713db6083a165f3c7d56d7f06186e
Leonid Bobrov <mazocomp@disroot.org>
2018-09-30 18:13:27 +0300

    Support DragonFlyBSD and prune unused variables.

--- toxcore/LAN_discovery.c
+++ toxcore/LAN_discovery.c
@@ -119,7 +119,7 @@ static void fetch_broadcast_info(uint16_t port)
     }
 }
 
-#elif defined(__linux__) || defined(__FreeBSD__)
+#elif defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
 
 #include <netinet/in.h>
 #include <sys/ioctl.h>
@@ -131,7 +131,7 @@ static void fetch_broadcast_info(uint16_t port)
 #include <linux/netdevice.h>
 #endif
 
-#ifdef __FreeBSD__
+#if defined(__FreeBSD__) || defined(__DragonFly__)
 #include <net/if.h>
 #endif
 
