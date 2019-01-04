ebf3a82de8709270cb46266f1c06a3c1cee0649d
Maxim Biro <nurupo.contributions@gmail.com>
2019-01-03 20:46:11 -0500

    Use new WineHQ Debian package repository key

--- other/docker/windows/get_packages.sh
+++ other/docker/windows/get_packages.sh
@@ -43,12 +43,17 @@ if [ "${SUPPORT_TEST}" = "true" ]; then
 
     # Add Wine package repository to use the latest Wine
     echo "deb https://dl.winehq.org/wine-builds/debian/ stretch main" >> /etc/apt/sources.list
-    curl -o Release.key https://dl.winehq.org/wine-builds/Release.key
+    curl -o Release.key https://dl.winehq.org/wine-builds/winehq.key
+    # Verify against a known good key fingerprint. --dry-run makes it so we don't actually import the key.
+    if ! gpg --batch --dry-run --import --import-options import-show --fingerprint Release.key | grep 'D43F 6401 4536 9C51 D786  DDEA 76F1 A20F F987 672F'; then
+        echo "Error: WineHQ's Debian package repository key fingerprint didn't match the expected one. Exiting."
+        exit 1
+    fi
     apt-key add Release.key
 
     dpkg --add-architecture i386
     apt-get update
-    apt-get install -y --allow-unauthenticated \
+    apt-get install -y \
         wine-devel \
         wine-devel-amd64 \
         wine-devel-dbg \
