c1a2ea3309969608a5553c34fa4199b05f20abc2
iphydf <iphydf@users.noreply.github.com>
2020-05-04 02:19:14 +0100

    Use bash arrays instead of strings for static analysis scripts.
    These are more robust wrt. spaces in names.

--- other/analysis/gen-file.sh
+++ other/analysis/gen-file.sh
@@ -1,44 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
-CPPFLAGS="$CPPFLAGS -DMIN_LOGGER_LEVEL=LOGGER_LEVEL_TRACE"
-CPPFLAGS="$CPPFLAGS -isystem /usr/include/opus"
-CPPFLAGS="$CPPFLAGS -Iauto_tests"
-CPPFLAGS="$CPPFLAGS -Iother"
-CPPFLAGS="$CPPFLAGS -Iother/bootstrap_daemon/src"
-CPPFLAGS="$CPPFLAGS -Iother/fun"
-CPPFLAGS="$CPPFLAGS -Itesting"
-CPPFLAGS="$CPPFLAGS -Itoxcore"
-CPPFLAGS="$CPPFLAGS -Itoxav"
-CPPFLAGS="$CPPFLAGS -Itoxencryptsave"
+CPPFLAGS="-DMIN_LOGGER_LEVEL=LOGGER_LEVEL_TRACE"
+CPPFLAGS+=("-isystem" "/usr/include/opus")
+CPPFLAGS+=("-Iauto_tests")
+CPPFLAGS+=("-Iother")
+CPPFLAGS+=("-Iother/bootstrap_daemon/src")
+CPPFLAGS+=("-Iother/fun")
+CPPFLAGS+=("-Itesting")
+CPPFLAGS+=("-Itesting/groupchats")
+CPPFLAGS+=("-Itoxcore")
+CPPFLAGS+=("-Itoxav")
+CPPFLAGS+=("-Itoxencryptsave")
 
-LDFLAGS="$LDFLAGS -lopus -lsodium -lvpx -lpthread -lconfig"
+LDFLAGS=("-lopus" "-lsodium" "-lvpx" "-lpthread" "-lconfig")
+LDFLAGS+=("-fuse-ld=gold")
+LDFLAGS+=("-Wl,--detect-odr-violations")
+LDFLAGS+=("-Wl,--warn-common")
+LDFLAGS+=("-Wl,--warn-execstack")
+LDFLAGS+=("-Wl,-z,noexecstack")
+LDFLAGS+=("-Wl,-z,now")
 
 put() {
   if [ "$SKIP_LINES" = "" ]; then
-    echo "#line 1 \"$1\"" >> amalgamation.cc
+    echo "#line 1 \"$1\"" >>amalgamation.cc
   fi
-  cat "$1" >> amalgamation.cc
+  cat "$1" >>amalgamation.cc
 }
 
 putmain() {
-  echo "namespace $(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g') {" >> amalgamation.cc
+  echo "namespace ${1//[^a-zA-Z0-9_]/_} {" >>amalgamation.cc
   if [ "$SKIP_LINES" = "" ]; then
-    echo "#line 1 \"$1\"" >> amalgamation.cc
+    echo "#line 1 \"$1\"" >>amalgamation.cc
   fi
-  sed -e 's/^int main(/static &/' "$1" >> amalgamation.cc
-  echo "} //  namespace $(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g')" >> amalgamation.cc
+  sed -e 's/^int main(/static &/' "$1" >>amalgamation.cc
+  echo "} //  namespace ${1//[^a-zA-Z0-9_]/_}" >>amalgamation.cc
 }
 
 callmain() {
-  echo "  call($(echo "$1" | sed -e 's/[^a-zA-Z0-9_]/_/g')::main, argc, argv);" >> amalgamation.cc
+  echo "  call(${1//[^a-zA-Z0-9_]/_}::main, argc, argv);" >>amalgamation.cc
 }
 
-:> amalgamation.cc
+: >amalgamation.cc
 
-echo "#include <algorithm>" >> amalgamation.cc
-echo "#include <cstdio>" >> amalgamation.cc
-echo "#include <memory>" >> amalgamation.cc
-echo "#include <random>" >> amalgamation.cc
+echo "#include <algorithm>" >>amalgamation.cc
+echo "#include <cstdio>" >>amalgamation.cc
+echo "#include <memory>" >>amalgamation.cc
+echo "#include <random>" >>amalgamation.cc
 
 put auto_tests/check_compat.h
 
@@ -53,33 +60,35 @@ FIND_QUERY="$FIND_QUERY -and -not -name av_test.c"
 FIND_QUERY="$FIND_QUERY -and -not -name dht_test.c"
 FIND_QUERY="$FIND_QUERY -and -not -name version_test.c"
 
-(for i in $(eval "$FIND_QUERY"); do
-  grep -o '#include <[^>]*>' "$i" \
-    | grep -E -v '<win|<ws|<iphlp|<libc|<mach/|<crypto_|<randombytes|<u.h>|<sys/filio|<linux'
-done) | sort -u >> amalgamation.cc
+readarray -t FILES <<<"$(eval "$FIND_QUERY")"
 
-echo 'namespace {' >> amalgamation.cc
-for i in $(eval "$FIND_QUERY"); do
+(for i in "${FILES[@]}"; do
+  grep -o '#include <[^>]*>' "$i" |
+    grep -E -v '<win|<ws|<iphlp|<libc|<mach/|<crypto_|<randombytes|<u.h>|<sys/filio|<linux'
+done) | sort -u >>amalgamation.cc
+
+echo 'namespace {' >>amalgamation.cc
+for i in "${FILES[@]}"; do
   if ! grep -q '^int main(' "$i"; then
     put "$i"
   fi
 done
 
-for i in $(eval "$FIND_QUERY"); do
+for i in "${FILES[@]}"; do
   if grep -q '^int main(' "$i"; then
     putmain "$i"
   fi
 done
 
-echo "static void call(int m(), int argc, char **argv) { m(); }" >> amalgamation.cc
-echo "static void call(int m(int, char **), int argc, char **argv) { m(argc, argv); }" >> amalgamation.cc
-echo '}  // namespace' >> amalgamation.cc
+echo "static void call(int m(), int argc, char **argv) { m(); }" >>amalgamation.cc
+echo "static void call(int m(int, char **), int argc, char **argv) { m(argc, argv); }" >>amalgamation.cc
+echo '}  // namespace' >>amalgamation.cc
 
-echo "int main(int argc, char **argv) {" >> amalgamation.cc
-for i in $(eval "$FIND_QUERY"); do
+echo "int main(int argc, char **argv) {" >>amalgamation.cc
+for i in "${FILES[@]}"; do
   if grep -q '^int main(' "$i"; then
     callmain "$i"
   fi
 done
-echo "  return 0;" >> amalgamation.cc
-echo "}" >> amalgamation.cc
+echo "  return 0;" >>amalgamation.cc
+echo "}" >>amalgamation.cc
