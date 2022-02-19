6a6bc029de629e68e124577daa16581475690949
iphydf <iphydf@users.noreply.github.com>
2022-02-19 11:43:16 +0000

    cleanup: Remove unused execution trace library.

--- other/analysis/gen-file.sh
+++ other/analysis/gen-file.sh
@@ -55,7 +55,6 @@ FIND_QUERY="$FIND_QUERY -and -not -name amalgamation.cc"
 FIND_QUERY="$FIND_QUERY -and -not -name av_test.c"
 FIND_QUERY="$FIND_QUERY -and -not -name cracker.c"
 FIND_QUERY="$FIND_QUERY -and -not -name dht_test.c"
-FIND_QUERY="$FIND_QUERY -and -not -name trace.cc"
 FIND_QUERY="$FIND_QUERY -and -not -name version_test.c"
 FIND_QUERY="$FIND_QUERY -and -not -wholename './testing/fuzzing/*'"
 
