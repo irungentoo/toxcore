00ff078f9192928f70275cc67f29f448f4e3fa98
iphydf <iphydf@users.noreply.github.com>
2023-12-26 00:20:57 +0000

    cleanup: Use target_link_libraries directly in cmake.
    Instead of using `target_link_modules`, which does magic that we no
    longer need, because we only have 1 library we install, and all binaries
    we build link statically because they need access to internal symbols.

--- cmake/ModulePackage.cmake
+++ cmake/ModulePackage.cmake
@@ -70,7 +70,7 @@ function(add_module lib)
 endfunction()
 
 function(install_module lib)
-  if(ENABLE_SHARED)
+  if(TARGET ${lib}_shared)
     set_target_properties(${lib}_shared PROPERTIES
       VERSION ${SOVERSION}
       SOVERSION ${SOVERSION_MAJOR}
@@ -80,7 +80,7 @@ function(install_module lib)
       LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
       ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
   endif()
-  if(ENABLE_STATIC)
+  if(TARGET ${lib}_static)
     install(TARGETS ${lib}_static
       RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
       LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
@@ -112,60 +112,4 @@ function(install_module lib)
 
     install(FILES ${header} ${ARGN})
   endforeach()
-endfunction()
-
-function(target_link_modules target)
-  # If the target we're adding dependencies to is a shared library, add it to
-  # the set of targets.
-  if(TARGET ${target}_shared)
-    set(_targets ${_targets} ${target}_shared)
-    # Shared libraries should first try to link against other shared libraries.
-    set(${target}_shared_primary shared)
-    # If that fails (because the shared target doesn't exist), try linking
-    # against the static library. This requires the static library's objects to
-    # be PIC.
-    set(${target}_shared_secondary static)
-  endif()
-  # It can also be a static library at the same time.
-  if(TARGET ${target}_static)
-    set(_targets ${_targets} ${target}_static)
-    # Static libraries aren't actually linked, but their dependencies are
-    # recorded by "linking" them. If we link an executable to a static library,
-    # we want to also link statically against its transitive dependencies.
-    set(${target}_static_primary static)
-    # If a dependency doesn't exist as static library, we link against the
-    # shared one.
-    set(${target}_static_secondary shared)
-  endif()
-  # If it's neither, then it's an executable.
-  if(NOT _targets)
-    set(_targets ${_targets} ${target})
-    # Executables preferably link against static libraries, so they are
-    # standalone and can be shipped without any external dependencies. As a
-    # frame of reference: tests become roughly 600-800K binaries instead of
-    # 50-100K on x86_64 Linux.
-    set(${target}_primary static)
-    set(${target}_secondary shared)
-  endif()
-
-  foreach(dep ${ARGN})
-    foreach(_target ${_targets})
-      if(TARGET ${dep}_${${_target}_primary})
-        target_link_libraries(${_target} PRIVATE ${dep}_${${_target}_primary})
-      elseif(TARGET ${dep}_${${_target}_secondary})
-        target_link_libraries(${_target} PRIVATE ${dep}_${${_target}_secondary})
-      else()
-        # We record the modules linked to this target, so that we can collect
-        # them later when linking a composed module.
-        list(FIND LINK_MODULES ${dep} _index)
-        if(_index EQUAL -1)
-          set(LINK_MODULES ${LINK_MODULES} ${dep})
-        endif()
-
-        target_link_libraries(${_target} PRIVATE ${dep})
-      endif()
-    endforeach()
-  endforeach()
-
-  set(${target}_LINK_MODULES ${${target}_LINK_MODULES} ${LINK_MODULES} PARENT_SCOPE)
 endfunction()
