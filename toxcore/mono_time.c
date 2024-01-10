TokTok/c-toxcore

refactor: Assign malloc return to a local variable first.

toxcore/mono_time.c
@@ -119,18 +119,20 @@ Mono_Time *mono_time_new(const Memory *mem, mono_time_current_time_cb *current_t
     }
 
 #ifndef ESP_PLATFORM
-    mono_time->time_update_lock = (pthread_rwlock_t *)mem_alloc(mem, sizeof(pthread_rwlock_t));
+    pthread_rwlock_t *mutex = (pthread_rwlock_t *)mem_alloc(mem, sizeof(pthread_rwlock_t));
 
-    if (mono_time->time_update_lock == nullptr) {
+    if (mutex == nullptr) {
         mem_delete(mem, mono_time);
         return nullptr;
     }
 
-    if (pthread_rwlock_init(mono_time->time_update_lock, nullptr) != 0) {
-        mem_delete(mem, mono_time->time_update_lock);
+    if (pthread_rwlock_init(mutex, nullptr) != 0) {
+        mem_delete(mem, mutex);
         mem_delete(mem, mono_time);
         return nullptr;
     }
+
+    mono_time->time_update_lock = mutex;
 #endif
 
     mono_time_set_current_time_callback(mono_time, current_time_callback, user_data);

toxcore/network.c
@@ -1796,12 +1796,12 @@ int32_t net_getipport(const Memory *mem, const char *node, IP_Port **res, int to
 
 #ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
     if ((true)) {
-        *res = (IP_Port *)mem_alloc(mem, sizeof(IP_Port));
-        assert(*res != nullptr);
-        IP_Port *ip_port = *res;
-        ip_port->ip.ip.v4.uint32 = net_htonl(0x7F000003); // 127.0.0.3
-        ip_port->ip.family = *make_tox_family(AF_INET);
+        IP_Port *tmp = (IP_Port *)mem_alloc(mem, sizeof(IP_Port));
+        assert(tmp != nullptr);
+        tmp->ip.ip.v4.uint32 = net_htonl(0x7F000003); // 127.0.0.3
+        tmp->ip.family = *make_tox_family(AF_INET);
 
+        *res = tmp;
         return 1;
     }
 #endif
@@ -1838,14 +1838,13 @@ int32_t net_getipport(const Memory *mem, const char *node, IP_Port **res, int to
         return 0;
     }
 
-    *res = (IP_Port *)mem_valloc(mem, count, sizeof(IP_Port));
+    IP_Port *ip_port = (IP_Port *)mem_valloc(mem, count, sizeof(IP_Port));
 
-    if (*res == nullptr) {
+    if (ip_port == nullptr) {
         freeaddrinfo(infos);
+        *res = nullptr;
         return -1;
     }
-
-    IP_Port *ip_port = *res;
 
     for (struct addrinfo *cur = infos; cur != nullptr; cur = cur->ai_next) {
         if (cur->ai_socktype && type > 0 && cur->ai_socktype != type) {
@@ -1877,6 +1876,7 @@ int32_t net_getipport(const Memory *mem, const char *node, IP_Port **res, int to
 
     freeaddrinfo(infos);
 
+    *res = ip_port;
     return count;
 }
 

irungentoo/toxcore

toxcore/mono_time.c
@@ -119,18 +119,20 @@ Mono_Time *mono_time_new(const Memory *mem, mono_time_current_time_cb *current_t
     }
 
 #ifndef ESP_PLATFORM
-    mono_time->time_update_lock = (pthread_rwlock_t *)mem_alloc(mem, sizeof(pthread_rwlock_t));
+    pthread_rwlock_t *rwlock = (pthread_rwlock_t *)mem_alloc(mem, sizeof(pthread_rwlock_t));
 
-    if (mono_time->time_update_lock == nullptr) {
+    if (rwlock == nullptr) {
         mem_delete(mem, mono_time);
         return nullptr;
     }
 
-    if (pthread_rwlock_init(mono_time->time_update_lock, nullptr) != 0) {
-        mem_delete(mem, mono_time->time_update_lock);
+    if (pthread_rwlock_init(rwlock, nullptr) != 0) {
+        mem_delete(mem, rwlock);
         mem_delete(mem, mono_time);
         return nullptr;
     }
+
+    mono_time->time_update_lock = rwlock;
 #endif
 
     mono_time_set_current_time_callback(mono_time, current_time_callback, user_data);

toxcore/network.c
@@ -1796,12 +1796,14 @@ int32_t net_getipport(const Memory *mem, const char *node, IP_Port **res, int to
 
 #ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
     if ((true)) {
-        *res = (IP_Port *)mem_alloc(mem, sizeof(IP_Port));
-        assert(*res != nullptr);
-        IP_Port *ip_port = *res;
+        IP_Port *ip_port = (IP_Port *)mem_alloc(mem, sizeof(IP_Port));
+        if (ip_port == nullptr) {
+            abort();
+        }
         ip_port->ip.ip.v4.uint32 = net_htonl(0x7F000003); // 127.0.0.3
         ip_port->ip.family = *make_tox_family(AF_INET);
 
+        *res = ip_port;
         return 1;
     }
 #endif
@@ -1838,14 +1840,15 @@ int32_t net_getipport(const Memory *mem, const char *node, IP_Port **res, int to
         return 0;
     }
 
-    *res = (IP_Port *)mem_valloc(mem, count, sizeof(IP_Port));
+    IP_Port *ip_port = (IP_Port *)mem_valloc(mem, count, sizeof(IP_Port));
 
-    if (*res == nullptr) {
+    if (ip_port == nullptr) {
         freeaddrinfo(infos);
+        *res = nullptr;
         return -1;
     }
 
-    IP_Port *ip_port = *res;
+    *res = ip_port;
 
     for (struct addrinfo *cur = infos; cur != nullptr; cur = cur->ai_next) {
         if (cur->ai_socktype && type > 0 && cur->ai_socktype != type) {